172facb1f490e2eff31333f3e3d1dcbf58b9a003
Daytona <somosinnovacion.digitales@gmail.com>
2025-01-16 16:57:22 -0500

    cambio 2

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const BaileysProvider = require('@bot-whatsapp/provider/baileys');
 const MySQLAdapter = require('@bot-whatsapp/database/mysql'); // Asegúrate de que esta es la importación correcta
 const mysql = require('mysql'); // Importar el módulo mysql
 const { delay } = require('@whiskeysockets/baileys');
+const { chromium } = require('playwright'); // Use require instead of import
 
 const MYSQL_DB_HOST = 'localhost';
 const MYSQL_DB_USER = 'Miller';
@@ -15,6 +16,157 @@ const NumVendor = '51962196883'; // Número de WhatsApp Business
 let nombreGlobal = ''; // Variable global para almacenar el nombre
 let clienteGlobal = ''; // Variable global para almacenar el nombre del cliente
 
+// Flujo para calificación
+const flowCalificacion = addKeyword('calificacion')
+    .addAnswer('Por favor califica nuestro servicio de 1 a 5 estrellas. 🌟', { capture: true }, async (ctx, { flowDynamic, fallBack }) => {
+        const calificacion = parseInt(ctx.body);
+        if (isNaN(calificacion) || calificacion < 1 || calificacion > 5) {
+            return fallBack();  
+        }
+        else {
+
+        const numero = ctx.from;
+        console.log("Calificación del cliente:", calificacion);
+        console.log("Número del cliente:", numero);
+
+        const sql = 'INSERT INTO calificaciones (numero, calificacion) VALUES (?, ?)';
+        const values = [numero, calificacion];
+
+        const connection = mysql.createConnection({
+            host: MYSQL_DB_HOST,
+            user: MYSQL_DB_USER,
+            password: MYSQL_DB_PASSWORD,
+            database: MYSQL_DB_NAME,
+            port: MYSQL_DB_PORT,
+        });
+
+        connection.query(sql, values, (err, results) => {
+            if (err) {
+                console.error('Error al insertar calificación: ' + err.stack);
+                return;
+            }
+            console.log('Calificación insertada con id: ' + results.insertId);
+        });
+
+        connection.end();
+        await flowDynamic('¡Gracias por tu calificación! 🌟');
+    }
+    });
+
+// Flujo para búsqueda de productos para usuarios registrados
+const flowEnlace = addKeyword('USUARIOS_REGISTRADOS')
+    .addAnswer('¿Qué producto deseas comprar? 🛍️', { capture: true }, async (ctx, { flowDynamic, gotoFlow }) => {
+        const NomProd = ctx.body;
+
+        function generarEnlaceDeBusqueda(palabraClave) {
+            const enlaceBusqueda = `https://daytonaautopartes.com/busqueda?s=${encodeURIComponent(palabraClave)}`;
+            console.log("Enlace de búsqueda:", enlaceBusqueda);
+            return enlaceBusqueda;
+        }
+        await flowDynamic('Procesando tu solicitud...⏳');
+        const palabra = NomProd;
+        const enlaceCliente = generarEnlaceDeBusqueda(palabra);
+
+        const browser = await chromium.launch(
+            { headless: true }
+        );
+        const page = await browser.newPage();
+        await page.goto(enlaceCliente);
+        
+        const productos = await page.$$eval('article.product-miniature', (results) => results.map((el) => {
+            const title = el.querySelector('h3.product-title a').innerText;
+            if (!title) return null;
+        
+            const image = el.querySelector('img').src;
+            const price = el.querySelector('span.price').innerText;
+
+            const link = el.querySelector('h3.product-title a').href;
+        
+            return { title, image, price, link };
+        }));
+        
+        await browser.close();
+        await flowDynamic('🚨Por favor verificar las APLICACIONES si el nombre del producto no corresponde a su modelo de vehiculo🚨');
+        if (productos.length > 0) {
+            for (const producto of productos) {
+                if (producto) {
+                    const mensaje = `🚗 *Producto:* ${producto.title}\n💲 *Precio:* ${producto.price}\n🖼️ *Imagen:* ${producto.image}\n 🛒*Comprar:* ${producto.link}`;                
+                    await flowDynamic(mensaje);
+
+                  
+                }
+            }
+            let url1 = `https://api.whatsapp.com/send?phone=${NumVendor}&text=Hola, Soy ${clienteGlobal} encontre  *${palabra}* en la pagina web, me podrias ayudar`;
+            let encodedUrl1 = url1.replace(/ /g, '+');
+            await flowDynamic(`Si no encontro el producto que deseas ingresa aqui para hablar con un agente de ventas📲: ${encodedUrl1}`, {delay: 10000})
+        } else {
+            let url = `https://api.whatsapp.com/send?phone=${NumVendor}&text=Hola, Soy ${clienteGlobal} no encontre  *${palabra}* en la pagina web, me podrias ayudar`;
+            let encodedUrl = url.replace(/ /g, '+');
+            await flowDynamic(`No hay resultados por favor contactar con un agente de ventas aquí🔗:${encodedUrl} `, {delay: 10000});
+            
+        }
+       
+       return gotoFlow(flowCalificacion);
+    });
+
+// Flujo para búsqueda de productos para usuarios no registrados
+const flowEnlace_two = addKeyword('@')
+    .addAnswer('Para un mejor resultado por favor escribe el nombre de tu producto más el modelo de vehículo. 🚗🔧')
+    .addAnswer('¿Qué producto deseas comprar? 🛍️', { capture: true }, async (ctx, { flowDynamic, gotoFlow }) => {
+        const NomProd = ctx.body;
+
+        function generarEnlaceDeBusqueda(palabraClave) {
+            const enlaceBusqueda = `https://daytonaautopartes.com/busqueda?s=${encodeURIComponent(palabraClave)}`;
+            console.log("Enlace de búsqueda:", enlaceBusqueda);
+            return enlaceBusqueda;
+        }
+        await flowDynamic('Procesando tu solicitud...⏳');
+        const palabra = NomProd;
+        const enlaceCliente = generarEnlaceDeBusqueda(palabra);
+
+        const browser = await chromium.launch(
+            { headless: true }
+        );
+        const page = await browser.newPage();
+        await page.goto(enlaceCliente);
+        
+        const productos = await page.$$eval('article.product-miniature', (results) => results.map((el) => {
+            const title = el.querySelector('h3.product-title a').innerText;
+            if (!title) return null;
+        
+            const image = el.querySelector('img').src;
+            const price = el.querySelector('span.price').innerText;
+
+            const link = el.querySelector('h3.product-title a').href;
+        
+            return { title, image, price, link };
+        }));
+        
+        await browser.close();
+        await flowDynamic('🚨Por favor verificar las APLICACIONES si el nombre del prodcto no corresponde a su modelo de vehiculo🚨');
+        if (productos.length > 0) {
+            for (const producto of productos) {
+                if (producto) {
+                    const mensaje = `🚗 *Producto:* ${producto.title}\n💲 *Precio:* ${producto.price}\n🖼️ *Imagen:* ${producto.image}\n 🛒*Comprar:* ${producto.link}`;                
+                    await flowDynamic(mensaje);
+
+                  
+                }
+            }
+            let url1 = `https://api.whatsapp.com/send?phone=${NumVendor}&text=Hola, Soy ${clienteGlobal} encontre  *${palabra}* en la pagina web, me podrias ayudar?`;
+            let encodedUrl1 = url1.replace(/ /g, '+');
+            await flowDynamic(`Si no encontro el producto que deseas ingresa aqui para hablar con un agente de ventas📲: ${encodedUrl1}`, {delay: 10000})
+        } else {
+            let url = `https://api.whatsapp.com/send?phone=${NumVendor}&text=Hola, Soy ${clienteGlobal} no encontre  *${palabra}* en la pagina web, me podrias ayudar?`;
+            let encodedUrl = url.replace(/ /g, '+');
+            await flowDynamic(`No hay resultados por favor contactar con un agente de ventas aquí🔗:${encodedUrl} `, {delay: 10000});
+        }
+
+        console.log("Enlace de búsqueda:", enlaceCliente);
+        return gotoFlow(flowCalificacion);
+    });
+
+// Flujo para obtener datos de usuarios no registrados
 const flowDatos = addKeyword('USUARIOS_NO_REGISTRADOS')
     .addAnswer('Es tu primera vez en nuestra tienda en línea. Por favor, proporciona tus datos para continuar. 📝')
     .addAnswer('Por favor, proporciona tu nombre completo:', { capture: true }, async (ctx) => {
@@ -59,49 +211,13 @@ const flowDatos = addKeyword('USUARIOS_NO_REGISTRADOS')
         connection.end();
     });
 
-const flowEnlace_two = addKeyword('@')
-    .addAnswer('Para un mejor resultado por favor escribe el nombre de tu producto más el modelo de vehículo. 🚗🔧')
-    .addAnswer('¿Qué producto deseas comprar? 🛍️', { capture: true }, async (ctx, { flowDynamic }) => {
-        const NomProd = ctx.body;
-
-        function generarEnlaceDeBusqueda(palabraClave) {
-            const enlaceBusqueda = `https://daytonaautopartes.com/busqueda?s=${encodeURIComponent(palabraClave)}`;
-            console.log("Enlace de búsqueda:", enlaceBusqueda);
-            return enlaceBusqueda;
-        }
-
-        const palabra = NomProd;
-        const enlaceCliente = generarEnlaceDeBusqueda(palabra);
-        console.log("Enlace de búsqueda:", enlaceCliente);
-        await flowDynamic(`Ingrese aquí para ver su producto: ${enlaceCliente} 🔗`);
-    });
-
-const flowEnlace = addKeyword('USUARIOS_REGISTRADOS')
-    .addAnswer('¿Qué producto deseas comprar? 🛍️', { capture: true }, async (ctx, { flowDynamic }) => {
-        const NomProd = ctx.body;
-
-        function generarEnlaceDeBusqueda(palabraClave) {
-            const enlaceBusqueda = `https://daytonaautopartes.com/busqueda?s=${encodeURIComponent(palabraClave)}`;
-            console.log("Enlace de búsqueda:", enlaceBusqueda);
-            return enlaceBusqueda;
-        }
-
-        const palabra = NomProd;
-        const enlaceCliente = generarEnlaceDeBusqueda(palabra);
-        console.log("Enlace de búsqueda:", enlaceCliente);
-        await flowDynamic(`Ingrese aquí para ver su producto: ${enlaceCliente} 🔗`);
-        let url = `https://api.whatsapp.com/send?phone=${NumVendor}&text=Hola, Soy ${clienteGlobal} no encontre  *${NomProd}* en la pagina web, me podrias ayudar?`;
-        let encodedUrl = url.replace(/ /g, '+');
-    
-        await flowDynamic(`Si necesita contactar con un agente de ventas ingresar aqui por favor: ${encodedUrl}`, {delay: 10000})
-        
-    });
-
+// Flujos principales
 const flowPrincipal = addKeyword(['hola', 'ole', 'alo', 'buenas', 'buena', 'ola'])
     .addAnswer('Recuerda que puedes comprar en nuestra tienda en línea. Es seguro y confiable. 🛒✨')
-    .addAnswer('Para crear una cuenta en nuestra página web y recibir super promociones y descuentos, ingresa al siguiente enlace: 🎁👇')
-    .addAnswer('https://daytonaautopartes.com/crear-cuenta')
+    .addAnswer('Para crear una cuenta en nuestra página web y recibir super promociones y descuentos, ingresa al siguiente enlace: 🎁👇', {delay:2000})
+    .addAnswer('https://daytonaautopartes.com/crear-cuenta', {delay: 2000})
     .addAnswer('Si deseas seguir la atención por este medio escribe "si" 📝', { capture: true }, async (ctx, { flowDynamic, gotoFlow }) => {
+        console.log(ctx);
         const numero = ctx.from;
 
         // Verificar si el número ya está registrado
@@ -144,7 +260,7 @@ const main = async () => {
         port: MYSQL_DB_PORT,
     });
 
-    const adapterFlow = createFlow([flowPrincipal, flowDatos, flowEnlace, flowEnlace_two]);
+    const adapterFlow = createFlow([flowPrincipal, flowDatos, flowEnlace, flowEnlace_two, flowCalificacion]);
     const adapterProvider = createProvider(BaileysProvider);
     createBot({
         flow: adapterFlow,
